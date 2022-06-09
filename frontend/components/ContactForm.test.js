b4942f66b8be1e56bdaceab46a549f8dd4a33f2e
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-09 17:15:50 -0400

    first four tests working

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -5,23 +5,39 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', () => {
-    render(<ContactForm />)
+    render(<ContactForm />);
 });
 
 test('renders the contact form header', () => {
-    render(<ContactForm />)
+    render(<ContactForm />);
 
     const header = screen.queryByText(/contact form/i);
 
     expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent(/contact form/i);
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    render(<ContactForm />);
 
+    const firstNameField = screen.getByLabelText(/first name*/i);
+    userEvent.type(firstNameField, '123');
+
+    const errorMessage = await screen.findAllByTestId('error');
+    expect(errorMessage).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
+    render(<ContactForm />);
 
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+        const errorMessage = screen.queryAllByTestId('error');
+        expect(errorMessage).toHaveLength(3);
+    })
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
